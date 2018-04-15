f2382600333987b369139045541a9e2b01388aa2
marco <gundul1@libero.it>
2018-04-15 19:12:41 +0200

    add 3 bugs with random speed and posizion

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
+const possibleY=[57, 140, 223];
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    const randY=1;
+    this.x=520;
+    this.y="";
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +18,14 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    if(this.x<520){
+      this.x+=dt*150*this.speed;
+    }else{
+      this.x=-105
+      randY=Math.random()*2.99;
+      this.speed=1+Math.random()*2.99;
+      this.y=possibleY[Math.floor(randY)];
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -29,8 +41,11 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+var en1 = new Enemy();
+var en2 = new Enemy();
+var en3 = new Enemy();
+var allEnemies = [en1, en2, en3];
+var player = new Enemy();
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
