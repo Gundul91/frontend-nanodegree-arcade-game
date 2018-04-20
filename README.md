0ec3015181b1108750587303fe9cc9731906e5b3
marco <gundul1@libero.it>
2018-04-20 18:54:13 +0200

    start add sounds

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 For launch the program the user can double click on the "index.html" file or drag and drop it on a browser
 
-## Instruction
+## Instructions
 
 In the first screen the player has to chose an avatar, then he as to use the arrows to move the player and try to take the gems without hit the bugs and arrive to the water. Every time he arrive to the water after he take some gems he get points (green=1, blue=3, orange=8) and restart from the start point with 2 new gems on the road instead if he hit a bug he lose the gems taken, 1 point and 1 life. When he reach 26/61/101 points the number of bugs is increases by one. After he lose all the lifes the game end and a window show the number of points reached.
 
--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 const possibleY = [57, 140, 223];
 const possibleX = [0, 101, 202, 303, 404];
 const possibleGem = ["images/Gem Green.png" , "images/Gem Blue.png" , "images/Gem Orange.png"];
@@ -16,7 +16,7 @@ const bugs_value = document.querySelector(".bugs_value");
 
 const result_score_value = document.querySelector(".result_score_value");
 
-//Base class fo all the entityes (Player, Enemys, Gems)
+//Base class for all the entities (Player, Enemys, Gems)
 class Entity {
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -29,7 +29,7 @@ class Enemy extends Entity {
         super();
         this.x = 520;
         this.y = "";
-        this.sprite = 'images/enemy-bug.png';
+        this.sprite = "images/enemy-bug.png";
     }
 
     // Update the enemy's position, required method for game
@@ -39,7 +39,7 @@ class Enemy extends Entity {
         if (this.x < 520) {
             this.x += dt * 150 * this.speed;
         } else { // if arrive to the end return to the left
-            this.x = -105
+            this.x = -105;
             let randY = Math.random() * 2.99;
             this.speed = 1 + Math.random() * 2.99;
             this.y = possibleY[Math.floor(randY)];
@@ -51,51 +51,51 @@ class Enemy extends Entity {
 class Player extends Entity {
     constructor() {
         super();
-        this.sprite = 'images/char-boy.png';
+        this.sprite = "images/char-boy.png";
     }
 
     // Update the player position  when arrive at the water or hit a bug
     update() {
-      // if arrive to the water
-      if (this.y < 57) {
-          this.start();
-          result += gemPoints;
-          gemPoints = 0;
-          score.textContent = result;
-          allGem.forEach(function(gem) {
-              gem.start();
-          });
-          // if arrive at 26/61/101 point add a bug
-          if (result > 25 && allEnemies.length < 4) {
-              bugs_value.textContent++;
-              allEnemies.push(new Enemy());
-          } else if (result > 60 && allEnemies.length < 5) {
-              bugs_value.textContent++;
-              allEnemies.push(new Enemy());
-          } else if (result > 100 && allEnemies.length < 6) {
-              bugs_value.textContent++;
-              allEnemies.push(new Enemy());
-          }
-      }
+        // if arrive to the water
+        if (this.y < 57) {
+            this.start();
+            result += gemPoints;
+            gemPoints = 0;
+            score.textContent = result;
+            allGem.forEach(function(gem) {
+                gem.start();
+            });
+            // if arrive at 26/61/101 point add a bug
+            if (result > 25 && allEnemies.length < 4) {
+                bugs_value.textContent++;
+                allEnemies.push(new Enemy());
+            } else if (result > 60 && allEnemies.length < 5) {
+                bugs_value.textContent++;
+                allEnemies.push(new Enemy());
+            } else if (result > 100 && allEnemies.length < 6) {
+                bugs_value.textContent++;
+                allEnemies.push(new Enemy());
+            }
+        }
 
-      allEnemies.forEach(function(enemie) {
-          //collisions control
-          if(enemie.y == this.y && enemie.x > (this.x - 50) && enemie.x < (this.x + 50)){
-              this.start();
-              result = result > 0 ? result - 1 : 0;
-              score.textContent = result;
-              gemPoints = 0;
-              lifes--;
-              lifes_value.textContent = lifes;
-              // if lifes are finished
-              if (lifes === 0) {
-                  result_score_value.textContent = result;
-                  cover.style.display = "block";
-                  result_box.style.display = "block";
-                  block_game = true;
-              }
-          }
-      }, this);
+        allEnemies.forEach(function(enemie) {
+            //collisions control
+            if(enemie.y == this.y && enemie.x > (this.x - 50) && enemie.x < (this.x + 50)){
+                this.start();
+                result = result > 0 ? result - 1 : 0;
+                score.textContent = result;
+                gemPoints = 0;
+                lifes--;
+                lifes_value.textContent = lifes;
+                // if lifes are finished
+                if (lifes === 0) {
+                    result_score_value.textContent = result;
+                    cover.style.display = "block";
+                    result_box.style.display = "block";
+                    block_game = true;
+                }
+            }
+        }, this);
     }
 
     // Handle input for player's movement
@@ -103,22 +103,22 @@ class Player extends Entity {
     handleInput(direction) {
         if (block_game === false) {
             switch(direction) {
-                case "left":
-                    if (this.x > 0)
-                        this.x -= 101;
-                    break;
-                case "right":
-                    if (this.x < 404)
-                        this.x += 101;
-                    break;
-                case "up":
-                    if (this.y > 0)
-                        this.y -= 83;
-                    break;
-                case "down":
-                    if (this.y < 389)
-                        this.y += 83;
-                    break;
+            case "left":
+                if (this.x > 0)
+                    this.x -= 101;
+                break;
+            case "right":
+                if (this.x < 404)
+                    this.x += 101;
+                break;
+            case "up":
+                if (this.y > 0)
+                    this.y -= 83;
+                break;
+            case "down":
+                if (this.y < 389)
+                    this.y += 83;
+                break;
             }
         }
     }
@@ -152,15 +152,15 @@ class Gem extends Entity {
         //collisions control
         if (this.x == player.x && this.y == player.y) {
             switch(this.sprite) {
-                case possibleGem[0]:
-                    gemPoints += 1;
-                    break;
-                case possibleGem[1]:
-                    gemPoints += 3;
-                    break;
-                case possibleGem[2]:
-                    gemPoints += 8;
-                    break;
+            case possibleGem[0]:
+                gemPoints += 1;
+                break;
+            case possibleGem[1]:
+                gemPoints += 3;
+                break;
+            case possibleGem[2]:
+                gemPoints += 8;
+                break;
             }
             this.x += 1000;
             this.y += 1000;
@@ -196,12 +196,12 @@ function declareEnemies(size) {
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method.
-document.addEventListener('keyup', function(e) {
+document.addEventListener("keyup", function(e) {
     const allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
@@ -211,9 +211,29 @@ let avatarSelector = document.querySelector(".avatars");
 let cover = document.querySelector(".cover");
 
 // Listener for the choice of the avatar
-avatarSelector.addEventListener('click', function(e) {
+avatarSelector.addEventListener("click", function(e) {
     player.sprite = e.path[0].getAttribute("src");
     player.start();
     cover.style.display = "none";
     this.style.display = "none";
 });
+
+function initSounds(){
+    // if initializeDefaultPlugins returns false, we cannot play sound in this browser
+    if (!createjs.Sound.initializeDefaultPlugins()) { return; }
+
+    var assetsPath = "sounds/";
+    var sounds = [
+        {src: "Snowfall.ogg", id: "music"},
+        {src: "applause.wav", id: "end"}
+    ];
+    createjs.Sound.alternateExtensions = ["mp3"];	// add other extensions to try loading if the src file extension is not supported
+    createjs.Sound.addEventListener("fileload", handleLoad);
+    createjs.Sound.registerSounds(sounds,assetsPath);
+}
+
+
+function handleLoad(event) {
+  createjs.Sound.play("sounds/Snowfall.ogg");
+  //createjs.Sound.play(event.src);
+}
